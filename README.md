4cda95926311c12566030cfb881aab5ef8e633b8
hogarth83 <beke.andras@gmail.com>
2020-03-06 21:10:43 +0100

    Modified create_env.sh, correct pip install

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # taxonomy_classification
+
+
+### Produce environment
+To produce conda enviroment run this bash script:
+
+    bash create_env.sh 
+
+This will create conda enviroment with all required dependencies and python packages
+
---
+++ create_env.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env
+
+source ~/anaconda3/etc/profile.d/conda.sh
+
+input_variable="taxonomy_classification"
+
+ENVS=$(conda env list | awk '{print $input_variable}' )
+
+if [[ $ENVS != *"$input_variable"* ]]; then
+
+  echo "# BASH: conda env create # source activate phd
+  name: $input_variable
+  dependencies:
+  - python=3.7.5
+  - pytorch
+  - pytorch-gpu
+  - numpy
+  - pandas
+  - scipy
+  - numpy
+  - scikit-learn
+  - pip
+  - pip:
+    - ktrain
+    - bs4
+    - dvc
+    - pandarallel
+    - wget">environment.yml
+
+
+  #list name of packages
+  echo "installing base packages"
+  conda env create -f=environment.yml
+  conda activate $input_variable
+  pip install -U spacy
+  python -m spacy download en_core_web_sm
+
+
+
+else
+   echo "$input_variable already exist!"
+   conda activate $input_variable
+fi
